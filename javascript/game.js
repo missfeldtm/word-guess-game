c35a0bd919162861e64fa84fcedf010a5f007f54
Mckenna <missfeldtm@gmail.com>
2019-04-19 21:44:22 -0500

    functional javascript for basic function

---
+++ javascript/game.js
@@ -0,0 +1,199 @@
+
+//word picking variables
+var words = ['beyonce', 'nsync', 'coldplay', 'nickelback','radiohead', 'paramore'];
+var playingWord = '';
+var wordArr = [];
+var blanks = 0;
+
+
+//Set GamePlay Variables
+var letterGuessed = 0;
+var correctGuesses = 0;
+var lives = 15;
+
+var wrongLetters = [];
+var underscoreDisplay = [];
+
+
+//using double word to allow one button press
+var letterChecker = ['a', 'b', 'c',
+    'd', 'e', 'f',
+    'g', 'h', 'i',
+    'j', 'k', 'l',
+    'm', 'n', 'o',
+    'p', 'q', 'r',
+    's', 't', 'u',
+    'v', 'w', 'x',
+    'y', 'z',' '
+];
+
+
+
+//grab variables to maniupulate from the DOM 
+
+var wrongGuesses = document.querySelector('.wrong-guess');
+var wordToGuess = document.querySelector('.underscores');
+var livesRemaining = document.querySelector('.lives');
+
+
+/**************************************************START GAME**************************************************/
+
+
+init();
+
+document.onkeyup = function (event) {
+  
+    var letterGuessed = event.key;
+    console.log(event.key);
+    //check to see if user has guessed letter already
+    for (var i = 0; i < letterChecker.length; i++) {
+        if (letterGuessed === letterChecker[i]) {
+            var eliminate = letterChecker.splice(i, 1);
+
+            compare(letterGuessed);
+            gameEnder();
+
+        }
+    }
+
+}
+
+
+/**************************************************FUNCTIONS**************************************************/
+
+//generates a word by using math.random
+
+function wordPicker() {
+
+    playingWord = words[Math.floor(Math.random() * words.length)];
+
+    //create an array from string with .split method
+
+    wordArr = playingWord.split('');
+    console.log(wordArr);
+
+    //generates the blanks array to be shown in DOM 
+    blanks = wordArr.length;
+
+    console.log(blanks);
+
+
+}
+
+//eliminates repeating code or resetting the game
+function dryEliminator() {
+
+    //sets everything to initial status so page refresh is not n eeded
+    letterGuessed = 0;
+    correctGuesses = 0;
+    lives = 15;
+
+    wrongLetters = [];
+    underscoreDisplay = [];
+    letterChecker = ['a', 'b', 'c',
+        'd', 'e', 'f',
+        'g', 'h', 'i',
+        'j', 'k', 'l',
+        'm', 'n', 'o',
+        'p', 'q', 'r',
+        's', 't', 'u',
+        'v', 'w', 'x',
+        'y', 'z'
+    ];
+
+  
+}
+
+
+
+function init() {
+
+    wordPicker();
+
+    dryEliminator();
+
+
+//generate word in the DOM MODEL
+    for (var i = 0; i < blanks; i++) {
+
+        underscoreDisplay.push('_');
+        wordToGuess.innerHTML = underscoreDisplay.join(' ');
+  
+
+    }
+
+    livesRemaining.innerHTML = lives;
+    wrongGuesses.innerHTML = wrongLetters;
+
+
+    console.log(playingWord);
+    console.log(wordArr);
+    console.log(blanks);
+    console.log(underscoreDisplay);
+
+
+}
+
+
+
+function compare(userGuess) {
+
+    //check to see if playing word was in user array
+    if (playingWord.indexOf(userGuess) > -1) {
+
+        for (var i = 0; i < blanks; i++) {
+            //Fills in right index with user key
+            if (wordArr[i] === userGuess) {
+                //add to all of the dom elements
+                correctGuesses++;
+                underscoreDisplay[i] = userGuess;
+                wordToGuess.innerHTML = underscoreDisplay.join(' ');
+            }
+        }
+
+    } else {
+        //when user guesses wrong
+        //push wrong gues into wrong guess array
+        wrongLetters.push(userGuess);
+        //loses a life
+        lives--;
+        //Changes HTML
+        livesRemaining.innerHTML = lives;
+        wrongGuesses.innerHTML = wrongLetters;
+    }
+
+}
+
+
+
+function gameEnder() {
+    // When number blanks if filled with right words then you win
+    if (correctGuesses === blanks) {
+
+
+        alert('You Win');
+        reset();
+    }
+    // When number of Guesses reaches 0 then You lose
+    else if (lives === 0) {
+
+        alert('You Lose');
+        reset();
+    }
+}
+
+
+
+function reset() {
+
+    wordPicker();
+
+    dryEliminator();
+
+    init();
+
+}
+
+
+
+
